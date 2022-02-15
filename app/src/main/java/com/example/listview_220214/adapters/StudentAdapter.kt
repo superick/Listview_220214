b5e4e5cc01b39afa09328a4c026d1606a90ddf93
superick <superick@naver.com>
2022-02-15 18:54:51 +0900

    Adapter 뼈대 작업 마무리

--- app/src/main/java/com/example/listview_220214/adapters/StudentAdapter.kt
+++ app/src/main/java/com/example/listview_220214/adapters/StudentAdapter.kt
@@ -1,12 +1,35 @@
 package com.example.listview_220214.adapters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.example.listview_220214.R
 import com.example.listview_220214.datas.Student
 import java.util.*
 
 class StudentAdapter(
         val mContext : Context,
         val resId : Int,
-        val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList) {
-}
+        val mList : ArrayList<Student>) : ArrayAdapter<Student>(mContext, resId, mList){
+
+        val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        val tempRow = convertView
+        if (tempRow == null) {
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+
+        }
+
+        val row = tempRow!!
+
+        return row
+
+    }
+
+
+
+        }
