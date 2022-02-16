2d486483cf65accbe2e23d143334616f21b2719d
superick <superick@naver.com>
2022-02-16 18:45:36 +0900

    학생목록 ArrayList 생성, 실제 목록 추가

--- app/src/main/java/com/example/listview_220214/MainActivity.kt
+++ app/src/main/java/com/example/listview_220214/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.listview_220214
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.listview_220214.datas.Student
 
 class MainActivity : AppCompatActivity() {
+
+    val mStudentList = ArrayList<Student>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add( Student("나는미니미", 1990 ))
+        mStudentList.add( Student("오수진", 1995 ))
+        mStudentList.add( Student("김삼순", 1997 ))
+        mStudentList.add( Student("김말년", 1985 ))
+        mStudentList.add( Student("이상택", 1990 ))
+        mStudentList.add( Student("박아영", 1995 ))
+        mStudentList.add( Student("이미진", 1998 ))
+
+
+
     }
 }
